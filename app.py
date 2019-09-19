94a20e322bbcba9c61bb528086dcb4ce8db4c87b
Ike <ikeholzmann@gmail.com>
2019-09-19 14:14:15 -0700

    Add comments and docstring

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
+    """Render index with query response from GIF API if query string contains a response from request.args.get('search').
+    If no search query, pass 'defaultQ' to the API for a default response."""
     q = request.args.get('search')
     if not q:
         q = defaultQ
@@ -17,14 +19,14 @@ def index():
 
     if r.status_code == 200:
         gifs = json.loads(r.content)
-        title = q + " GIFs | Graphics Interchange Format Search"
+        title = q + " GIFs | Graphics Interchange Format Search" #Dynamic title
         return render_template(
             'index.html',
             gifs=gifs["results"],
             title=title
             )
     else:
-        return "Request error code: " + r.status_code
+        return "Request error code: " + r.status_code #If error status from API
 
 if __name__ == '__main__':
     app.run(debug=True)
